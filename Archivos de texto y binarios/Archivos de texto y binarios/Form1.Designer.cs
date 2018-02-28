80fb6cbbdc6f104b9c790ad5d202f42c2177e89d
KarinaDell <36238027+KarinaDell@users.noreply.github.com>
2018-02-28 13:37:01 -0600

    Add files via upload

---
+++ Archivos de texto y binarios/Archivos de texto y binarios/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Archivos_de_texto_y_binarios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.ofd2 = new System.Windows.Forms.OpenFileDialog();
+            this.txtEditorial = new System.Windows.Forms.TextBox();
+            this.txtPaginas = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sfd1 = new System.Windows.Forms.SaveFileDialog();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(64, 89);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(203, 178);
+            this.txtDescripcion.TabIndex = 0;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(76, 55);
+            this.lblDescripcion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(104, 20);
+            this.lblDescripcion.TabIndex = 1;
+            this.lblDescripcion.Text = "Descricpión:";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrir.Location = new System.Drawing.Point(161, 289);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(106, 41);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.Text = "Abrir...";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // ofd2
+            // 
+            this.ofd2.FileName = "openFileDialog1";
+            // 
+            // txtEditorial
+            // 
+            this.txtEditorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditorial.Location = new System.Drawing.Point(490, 236);
+            this.txtEditorial.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEditorial.Name = "txtEditorial";
+            this.txtEditorial.Size = new System.Drawing.Size(179, 27);
+            this.txtEditorial.TabIndex = 3;
+            // 
+            // txtPaginas
+            // 
+            this.txtPaginas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPaginas.Location = new System.Drawing.Point(490, 201);
+            this.txtPaginas.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPaginas.Name = "txtPaginas";
+            this.txtPaginas.Size = new System.Drawing.Size(179, 27);
+            this.txtPaginas.TabIndex = 4;
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAutor.Location = new System.Drawing.Point(490, 166);
+            this.txtAutor.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(179, 27);
+            this.txtAutor.TabIndex = 5;
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGenero.Location = new System.Drawing.Point(490, 131);
+            this.txtGenero.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(179, 27);
+            this.txtGenero.TabIndex = 6;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitulo.Location = new System.Drawing.Point(490, 96);
+            this.txtTitulo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(179, 27);
+            this.txtTitulo.TabIndex = 7;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(509, 293);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(105, 36);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Título:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(411, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Género:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(426, 173);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Autor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(380, 204);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "No. páginas:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(405, 236);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Editorial:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(424, 43);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Archivo XML:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 23);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Imagen BMP:";
+            this.label7.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 364);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.txtPaginas);
+            this.Controls.Add(this.txtEditorial);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.txtDescripcion);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Karina";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.OpenFileDialog ofd1;
+        private System.Windows.Forms.OpenFileDialog ofd2;
+        private System.Windows.Forms.TextBox txtEditorial;
+        private System.Windows.Forms.TextBox txtPaginas;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.SaveFileDialog sfd1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Archivos de texto y binarios/Archivos de texto y binarios/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Archivos_de_texto_y_binarios
+{
+    public partial class Form1 : Form
+    {
+        FileStream archivo;
+        BinaryReader br;
+        StreamWriter sr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+
+            ofd2.Filter = "Imagenes (*.BMP)|*.bmp";
+            ofd2.ShowDialog();
+            archivo = new FileStream(ofd2.FileName, FileMode.Open);
+            br = new BinaryReader(archivo);
+
+            leerDatos();
+        }
+
+        private void leerDatos()
+        {
+            string info = "";
+
+            archivo.Seek(2, SeekOrigin.Begin); 
+            info += "Tamaño real en bytes: " + br.ReadInt32() + Environment.NewLine;
+
+            archivo.Seek(18, SeekOrigin.Begin); 
+            info += "Ancho: " + br.ReadInt32() + Environment.NewLine;
+
+            archivo.Seek(22, SeekOrigin.Begin);
+            info += "Alto: " + br.ReadInt32() + Environment.NewLine;
+
+            archivo.Seek(28, SeekOrigin.Begin); 
+            info += "Bits p/pixel: " + br.ReadInt32();
+
+            txtDescripcion.Text = info;
+
+            archivo.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            sfd1.ShowDialog();
+
+            archivo = new FileStream(sfd1.FileName, FileMode.Create);
+            sr = new StreamWriter(archivo);
+            Generar();
+        }
+
+        private void Generar()
+        {
+            sr.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+            sr.WriteLine("<Libro>");
+            sr.WriteLine("  <Titulo>" + txtTitulo.Text + "</Titulo>");
+            sr.WriteLine("  <Genero>" + txtGenero.Text + "</Genero>");
+            sr.WriteLine("  <Autor>" + txtAutor.Text + "</Autor>");
+            sr.WriteLine("  <Paginas>" + txtPaginas.Text + "</Paginas>");
+            sr.WriteLine("  <Editorial>" + txtEditorial.Text + "</Editorial>");
+            sr.WriteLine("</Libro>");
+
+            sr.Close();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
